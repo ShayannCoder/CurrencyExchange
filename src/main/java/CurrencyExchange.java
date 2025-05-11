ed31ddc4a99392190be012f59af12634ac356836
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-05-11 16:15:46 +0300

    worked on the API method, you can now enter the base currency and the amount and also choose the target currency to change your money int, also created a different method for the timestamp and calling it in the calculateExchange method, number formatting left to do.

--- src/main/java/CurrencyExchange.java
+++ src/main/java/CurrencyExchange.java
@@ -10,8 +10,7 @@ import org.json.JSONObject;
 public class CurrencyExchange {
     public static void main(String[] args){
         System.out.println(Exchange.calculateExchange());
-        Exchange ex1 = new Exchange();
-        System.out.println(ex1.timeStamp);
+        System.out.println();
     }
 }
 
@@ -19,22 +18,26 @@ class Exchange {
 
     private static final Scanner sc = new Scanner(System.in);
 
-    LocalDateTime DateTime = LocalDateTime.now();
-    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    String timeStamp = DateTime.format(format);
+    public static String getOriginalCurrency() {
+        System.out.print("What currency would you like to exchange? ");
+        return sc.nextLine();
+    }
 
     public static float moneyToBeExchanged() {
         System.out.print("please enter the amount of money you want to exchange: ");
         return sc.nextFloat();
     }
-        
 
-    public static float getRate() {
+    public static String getTargetCurrency() {
+        System.out.print("What currency would you like to change your money into? ");
+        sc.nextLine();
+        return sc.nextLine();
+    }
+
+    public static float getRate(String originalCurrency, String targetCurrency) {
         try {
-
-            
             String apiKey = "76abdb90da458f91cb518b75";
-            String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+            String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + originalCurrency;
             
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
@@ -44,10 +47,7 @@ class Exchange {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JSONObject json = new JSONObject(response.body());
 
-            System.out.print("What currency would you like to change to? ");
-            sc.nextLine();
-            String currencyType = sc.nextLine();
-            float rate = json.getJSONObject("conversion_rates").getFloat(currencyType);
+            float rate = json.getJSONObject("conversion_rates").getFloat(targetCurrency);
             return rate;
 
         } catch (Exception e) {
@@ -56,9 +56,20 @@ class Exchange {
         }
     }
 
+    public static String getDateTime() {
+    LocalDateTime DateTime = LocalDateTime.now();
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    String timeStamp = DateTime.format(format);
+    return timeStamp;
+    }
+
     public static float calculateExchange() {
+        String originalCurrency = getOriginalCurrency();
         float money = moneyToBeExchanged();
-        float rate = getRate();
+        String targetCurrency = getTargetCurrency();
+        float rate = getRate(originalCurrency, targetCurrency);
+        String timeDate = getDateTime();
+        System.out.println(timeDate);
         return money * rate;
     }
 }
